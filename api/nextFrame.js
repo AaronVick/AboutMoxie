8221c41e8a57e358daef5285fd87e7a8ed831d29
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 08:54:36 -0500

    Update nextFrame.js

--- api/nextFrame.js
+++ api/nextFrame.js
@@ -1,4 +1,15 @@
 export default function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'POST') {
     res.status(200).json({
       frames: [
