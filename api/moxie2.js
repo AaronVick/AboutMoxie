ac54e63faf721a83696c0c432d082b95bdfb513d
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 08:04:23 -0500

    Update moxie2.js

--- api/moxie2.js
+++ api/moxie2.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
         res.status(200).json({
             image: 'https://www.aaronvick.com/Moxie/moxie2.png',
             buttons: [
-                { label: 'Next', action: 'post', target: 'https://about-moxie.vercel.app/moxie3' },
+                { label: 'Next', action: 'post', target: 'https://about-moxie.vercel.app/api/moxie3' },
                 { label: 'Back', action: 'post', target: 'https://about-moxie.vercel.app' },
                 { label: 'Check Your Moxie', action: 'link', target: 'https://moxie-frames.airstack.xyz/ctar' },
                 { label: 'Aaron\'s FT', action: 'link', target: 'https://moxie-frames.airstack.xyz/sufta?t=235' }
