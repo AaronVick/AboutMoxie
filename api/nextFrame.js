c822ce0cd829bf270b46ce2d779f15299e71ec84
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 08:50:32 -0500

    Update nextFrame.js

--- api/nextFrame.js
+++ api/nextFrame.js
@@ -1,3 +1,19 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: 'API is working' });
+  if (req.method === 'POST') {
+    res.status(200).json({
+      frames: [
+        {
+          version: 'vNext',
+          image: 'https://www.aaronvick.com/Moxie/moxie2.png',
+          buttons: [
+            { label: 'Next' },
+            { label: 'Moxie.xyz', action: 'link', target: 'https://moxie.xyz' }
+          ],
+        }
+      ]
+    });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
