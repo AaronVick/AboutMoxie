1a8d862436515fd77f45ac593a66dd50e0503397
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 17:48:39 -0500

    Update nextFrame.js

--- api/nextFrame.js
+++ api/nextFrame.js
@@ -14,16 +14,27 @@ module.exports = (req, res) => {
   console.log('Request method:', req.method);
   console.log('Request body:', req.body);
 
-  res.status(200).json({
-    frames: [
-      {
-        version: 'vNext',
-        image: 'https://www.aaronvick.com/Moxie/moxie2.png',
-        buttons: [
-          { label: 'Next' },
-          { label: 'Moxie.xyz', action: 'link', target: 'https://moxie.xyz' }
-        ],
-      }
-    ]
-  });
+  // Farcaster frame response
+  const frameResponse = {
+    version: 'vNext',
+    ogImage: 'https://www.aaronvick.com/Moxie/moxie2.png',
+    frame: {
+      version: 'vNext',
+      image: 'https://www.aaronvick.com/Moxie/moxie2.png',
+      buttons: [
+        {
+          label: 'Next',
+          action: 'post'
+        },
+        {
+          label: 'Moxie.xyz',
+          action: 'link',
+          target: 'https://moxie.xyz'
+        }
+      ],
+      postUrl: 'https://about-moxie.vercel.app/api/nextFrame'
+    }
+  };
+
+  res.status(200).json(frameResponse);
 };
