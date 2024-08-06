e6ad6212d1ea973d8d5bba58c343b403288e61de
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 08:43:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-module.exports = {
-  async rewrites() {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
     return [
       {
-        source: '/api/nextFrame',
-        destination: '/api/nextFrame', // Point to the actual API route
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
       },
     ];
   },
 };
+
+module.exports = nextConfig;
