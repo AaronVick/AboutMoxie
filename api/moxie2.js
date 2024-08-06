5693f00c710e9db6f7bf63c00277fdd1646b6aac
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-06 08:01:48 -0500

    Create moxie2.js

---
+++ api/moxie2.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        res.status(200).json({
+            image: 'https://www.aaronvick.com/Moxie/moxie2.png',
+            buttons: [
+                { label: 'Next', action: 'post', target: 'https://about-moxie.vercel.app/moxie3' },
+                { label: 'Back', action: 'post', target: 'https://about-moxie.vercel.app' },
+                { label: 'Check Your Moxie', action: 'link', target: 'https://moxie-frames.airstack.xyz/ctar' },
+                { label: 'Aaron\'s FT', action: 'link', target: 'https://moxie-frames.airstack.xyz/sufta?t=235' }
+            ]
+        });
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
